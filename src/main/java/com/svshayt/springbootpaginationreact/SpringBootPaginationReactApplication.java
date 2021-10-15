553f4523a010bfe47439952687ecbb469ecb0076
ext.vshaytan <ext.vshaytan@tinkoff.ru>
2021-10-15 09:31:20 +0300

    Initial project

---
+++ src/main/java/com/svshayt/springbootpaginationreact/SpringBootPaginationReactApplication.java
@@ -0,0 +1,13 @@
+package com.svshayt.springbootpaginationreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPaginationReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPaginationReactApplication.class, args);
+    }
+
+}
